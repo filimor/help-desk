52f4a017025aa1170a1f44ce5b1ad5d8eb4e5f78
Filipe Moreira <filimor@posteo.com.br>
2019-10-27 20:25:16 -0300

    347 Autenticando usuario

--- valida_login.php
+++ valida_login.php
@@ -1,7 +1,35 @@
 <?php
 /* print_r($_GET);
-echo '<br />' . $_GET["email"] . '<br />' . $_GET["senha"] ; */
+echo '<br />' . $_GET["email"] . '<br />' . $_GET["senha"] ;
 
 print_r($_POST);
-echo '<br />' . $_POST["email"] . '<br />' . $_POST["senha"] ;
+echo '<br />' . $_POST["email"] . '<br />' . $_POST["senha"] ; */
+
+//usuarios do sistema
+$usuarios_app = array(
+	array('email' => 'adm@teste.com.br', 'senha' => '123456'),
+	array('email' => 'user@teste.com.br', 'senha' => 'abcd')
+);
+
+//verifica a autenticacao
+$usuario_autenticado = false;
+
+/*
+echo '<pre>';
+print_r($usuarios);
+echo '</pre>';
+*/
+
+foreach($usuarios_app as $user){
+	if($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']){
+		$usuario_autenticado = true;
+	}
+}
+
+if($usuario_autenticado){
+	echo 'Usuário autenticado';
+}else{
+	header('Location: index.php?login=erro');
+}
+
 ?>
