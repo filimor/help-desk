6361cd6db8dbceac7c6bbea61867c3ab2310f40c
Filipe Moreira <filimor@posteo.com.br>
2019-11-02 16:05:36 -0300

    355 Aplicando controle de perfil de usuarios

--- valida_login.php
+++ valida_login.php
@@ -13,14 +13,19 @@ print_r($_SESSION);
 echo '</pre>';
 */
 
-//usuarios do sistema
-$usuarios_app = array(
-	array('email' => 'adm@teste.com.br', 'senha' => '123456'),
-	array('email' => 'user@teste.com.br', 'senha' => 'abcd')
-);
-
 //verifica a autenticacao
 $usuario_autenticado = false;
+$usuario_id = null;
+$perfis = array(1 => 'Administrativo', 2 => 'Usuário');
+$usuario_perfil_id = null;
+
+//usuarios do sistema
+$usuarios_app = array(
+	array('id' => 1, 'email' => 'adm@teste.com.br', 'senha' => '1234', 'perfil_id' => 1),
+	array('id' => 2, 'email' => 'user@teste.com.br', 'senha' => '1234', 'perfil_id' => 1),
+  array('id' => 3, 'email' => 'jose@teste.com.br', 'senha' => '1234', 'perfil_id' => 2),
+  array('id' => 4, 'email' => 'maria@teste.com.br', 'senha' => '1234', 'perfil_id' => 3)
+);
 
 /*
 echo '<pre>';
@@ -31,12 +36,16 @@ echo '</pre>';
 foreach($usuarios_app as $user){
 	if($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']){
 		$usuario_autenticado = true;
+    $usuario_id = $user['id'];
+    $usuario_perfil_id = $user['perfil_id'];
 	}
 }
 
 if($usuario_autenticado){
 	echo 'Usuário autenticado';
     $_SESSION['autenticacao'] = 'SIM';
+    $_SESSION['id'] = $usuario_id;
+    $_SESSION['perfil_id'] = $usuario_perfil_id;
     header('Location: home.php');
 }else{
     $_SESSION['autenticacao'] = 'NAO';
